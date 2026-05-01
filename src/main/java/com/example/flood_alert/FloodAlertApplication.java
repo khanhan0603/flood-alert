88960a0747bf3d1adc0ff992839c65e4bb3c3bc7
An Luong <lmkhanhan@gmail.com>
2026-05-01 12:48:26 +0700

    Set up CI

---
+++ src/main/java/com/example/flood_alert/FloodAlertApplication.java
@@ -0,0 +1,13 @@
+package com.example.flood_alert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodAlertApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flood_alert/FloodAlertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flood_alert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloodAlertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
