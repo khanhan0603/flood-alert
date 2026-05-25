bcc0cfce944fac343d64b8356db5ffe32c2a9164
An Luong <lmkhanhan@gmail.com>
2026-05-25 18:11:22 +0700

    Fix WeatherDataRespository

--- src/main/java/com/example/flood_alert/repository/WeatherDataRepository.java
+++ src/main/java/com/example/flood_alert/repository/WeatherDataRepository.java
@@ -1,6 +1,5 @@
 package com.example.flood_alert.repository;
 
-import java.awt.print.Pageable;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
@@ -12,17 +11,41 @@ import org.springframework.data.repository.query.Param;
 import com.example.flood_alert.entity.Area;
 import com.example.flood_alert.entity.WeatherData;
 
-public interface WeatherDataRepository extends JpaRepository<WeatherData, UUID> {
-    boolean existsByAreaAndTime(Area area, LocalDateTime time);
+public interface WeatherDataRepository
+    extends JpaRepository<WeatherData, UUID> {
 
-    @Query("SELECT DISTINCT w.area.id FROM WeatherData w WHERE w.area.id IN :areaIds")
-    List<UUID> findDistinctAreaIdsByAreaIdIn(@Param("areaIds") List<UUID> areaIds);
+    boolean existsByAreaAndTime(
+        Area area,
+        LocalDateTime time
+    );
 
+    @Query("""
+        SELECT DISTINCT w.area.id
+        FROM WeatherData w
+        WHERE w.area.id IN :areaIds
+    """)
+    List<UUID> findDistinctAreaIdsByAreaIdIn(
+        @Param("areaIds") List<UUID> areaIds
+    );
+
+    @Query("""
+        SELECT COUNT(a)
+        FROM Area a
+        WHERE a.level = 2
+        AND a.lat IS NOT NULL
+        AND a.lon IS NOT NULL
+        AND NOT EXISTS (
+            SELECT 1
+            FROM WeatherData w
+            WHERE w.area.id = a.id
+        )
+    """)
     long countAreaWithoutWeatherData();
 
-   @Query("""
+    @Query("""
         SELECT COUNT(DISTINCT w.area.id)
         FROM WeatherData w
+        WHERE w.area.level = 2
     """)
     long countDistinctAreaId();
 }
