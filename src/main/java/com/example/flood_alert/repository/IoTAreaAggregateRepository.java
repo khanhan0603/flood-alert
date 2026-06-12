ff2dd6fbd5a9d73a6691aa1a68892f819209f702
An Luong <lmkhanhan@gmail.com>
2026-06-13 03:34:46 +0700

    Fix IoTAggregateRepository

--- src/main/java/com/example/flood_alert/repository/IoTAreaAggregateRepository.java
+++ src/main/java/com/example/flood_alert/repository/IoTAreaAggregateRepository.java
@@ -6,30 +6,37 @@ import java.util.UUID;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.flood_alert.entity.IoTAreaAggregates;
 
-import io.lettuce.core.dynamic.annotation.Param;
-
 public interface IoTAreaAggregateRepository extends JpaRepository<IoTAreaAggregates, UUID> {
     Optional<IoTAreaAggregates> findTopByAreaIdOrderByRecordedAtDesc(UUID areaId);
-    @Query(value = """
-            SELECT DISTINCT ON (ia.area_id)
-                ia.*
-            FROM iot_area_aggregates ia
-            ORDER BY ia.area_id, ia.recorded_at DESC
-            """, nativeQuery = true)
+
+    @Transactional(readOnly = true)
+    @Query("""
+                SELECT ia
+                FROM IoTAreaAggregates ia
+                JOIN FETCH ia.area
+                WHERE ia.recordedAt = (
+                    SELECT MAX(sub.recordedAt)
+                    FROM IoTAreaAggregates sub
+                    WHERE sub.area.id = ia.area.id
+                )
+            """)
     List<IoTAreaAggregates> findLatestAggregateOfEachArea();
 
     @Query("""
                 SELECT ia
                 FROM IoTAreaAggregates ia
-                JOIN FETCH ia.area
                 WHERE ia.area.id = :areaId
                 ORDER BY ia.recordedAt DESC
             """)
+    @EntityGraph(attributePaths = { "area" })
     Page<IoTAreaAggregates> findByAreaId(
             @Param("areaId") UUID areaId,
             Pageable pageable);
