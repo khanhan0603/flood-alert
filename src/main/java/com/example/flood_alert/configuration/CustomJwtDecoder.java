8536673aaea6a19a6b2268c47f900be4021f23de
An Luong <lmkhanhan@gmail.com>
2026-07-24 22:33:15 +0700

    Debug decode token

--- src/main/java/com/example/flood_alert/configuration/CustomJwtDecoder.java
+++ src/main/java/com/example/flood_alert/configuration/CustomJwtDecoder.java
@@ -1,14 +1,14 @@
 package com.example.flood_alert.configuration;
 
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
 import org.springframework.stereotype.Component;
-
-import javax.crypto.spec.SecretKeySpec;
 
 import com.example.flood_alert.repository.InvalidatedTokenRepository;
 
@@ -30,18 +30,19 @@ public class CustomJwtDecoder implements JwtDecoder {
 
         if (nimbusJwtDecoder == null) {
 
-            SecretKeySpec secretKeySpec =
-                    new SecretKeySpec(signedKey.getBytes(), "HS512");
+            SecretKeySpec secretKeySpec = new SecretKeySpec(signedKey.getBytes(), "HS512");
 
-            nimbusJwtDecoder =
-                    NimbusJwtDecoder.withSecretKey(secretKeySpec)
-                            .macAlgorithm(MacAlgorithm.HS512)
-                            .build();
+            nimbusJwtDecoder = NimbusJwtDecoder.withSecretKey(secretKeySpec)
+                    .macAlgorithm(MacAlgorithm.HS512)
+                    .build();
         }
 
         Jwt jwt = nimbusJwtDecoder.decode(token);
 
         String jti = jwt.getId();
+
+        System.out.println("JWT ID = " + jti);
+        System.out.println("Exists = " + invalidatedTokenRepository.existsByJwtId(jti));
 
         if (invalidatedTokenRepository.existsByJwtId(jti)) {
             throw new JwtException("Token has been invalidated");
