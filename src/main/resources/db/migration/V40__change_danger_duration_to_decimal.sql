711ae61bb4dd95ba920caca5a20e2bb75801f8d6
An Luong <lmkhanhan@gmail.com>
2026-08-09 19:53:13 +0700

    Fix danger duration minutes

--- src/main/resources/db/migration/V40__change_danger_duration_to_decimal.sql
+++ src/main/resources/db/migration/V40__change_danger_duration_to_decimal.sql
@@ -2,6 +2,6 @@ ALTER TABLE iot_area_aggregates
 ALTER COLUMN danger_duration_minutes TYPE NUMERIC(10,2)
 USING danger_duration_minutes::NUMERIC(10,2);
 
-ALTER TABLE area_risk_snapshot
+ALTER TABLE area_risk_snapshots
 ALTER COLUMN danger_duration_minutes TYPE NUMERIC(10,2)
 USING danger_duration_minutes::NUMERIC(10,2);
